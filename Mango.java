2e906f8c3d784670f8799bfe6067ea7af29469ae
Pawan G Tekale <121091609+Pawangtekale@users.noreply.github.com>
2025-06-08 19:38:14 +0530

    Add files via upload

---
+++ Mango.java
@@ -0,0 +1,19 @@
+class Mango 
+{
+	String name;
+	double price;
+	String origin;
+	
+	public Mango printName(){
+		System.out.println(name);
+		return this;
+	}
+	public Mango printPrice(){
+		System.out.println(price);
+		return this;
+	}
+	public Mango printOrigin(){
+		System.out.println(origin);
+		return this;
+	}
+}
---
+++ MangoDrive.java
@@ -0,0 +1,13 @@
+class MangoDrive 
+{
+	public static void main(String[] args) 
+	{
+		Mango m1=new Mango();
+		m1.name="Hapus";
+		m1.price=1200;
+		m1.origin="Rtnagiri";
+		
+		m1.printName().printPrice().printOrigin();
+		
+	}
+}
